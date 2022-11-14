2b5c1b2ec325242e0146dd46ed4d58172a80075c
nickbloo <nickbloo2@gmail.com>
2022-11-14 16:06:32 -0600

    log in function api

--- api/users.js
+++ api/users.js
@@ -34,13 +34,47 @@ usersRouter.post("/register", async (req, res, next) => {
             message: "Thank you for signing up!",
             token: token
         });
-        
+
     } catch ({name, message}) {
         next({name, message})
     }
 });
 
-// user login
+usersRouter.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+
+    if(!username || !password) {
+        next({
+            name: "Invalid login Error",
+            message: "Please enter a valid username and password"
+        })
+    }
+
+    try {
+        const user = await getUserByUsername(username);
+        const token = jwt.sign({
+            id: user.id,
+            username }, JWT_SECRET, {
+                expiresIn: "1w"
+        });
+
+        if (user && user.password === password) {
+            res.send({
+                message: "Logged in!",
+                token: token
+            })
+        } else {
+            next({
+                name: "Incorrect Login Error",
+                message: "Your username or password is incorrect"
+            })
+        }
+
+    } catch ({name, message}) {
+        next({name, message})
+    }
+});
+
 // user/me
 // user/:username/routines
 
