ffd9733c3b40d13340c21be583d99bcf71351764
nickbloo <nickbloo2@gmail.com>
2022-11-14 15:43:56 -0600

    checkout commit

--- api/activities.js
+++ api/activities.js
@@ -1,10 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const activitiesRouter = express.Router();
 
 module.exports = activitiesRouter;
-=======
-const router = express.Router();
-
-module.exports = router;
->>>>>>> 613644f619f441024e63eec1ba9f00833492aa70
--- api/routines.js
+++ api/routines.js
@@ -1,10 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const routinesRouter = express.Router();
 
 module.exports = routinesRouter;
-=======
-const router = express.Router();
-
-module.exports = router;
->>>>>>> 613644f619f441024e63eec1ba9f00833492aa70
