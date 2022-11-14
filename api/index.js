47da868887a5fe6dcef1fabb3b4dc182aee6d311
nickbloo <nickbloo2@gmail.com>
2022-11-14 14:53:01 -0600

    more api route changes

--- api/index.js
+++ api/index.js
@@ -6,15 +6,24 @@ const routinesRouter = require("./routines");
 const routineActivitiesRouter = require("./routineActivities");
 const { requireUser } = require("./utilities");
 
-// JWT SECRET and other requires relating to web token here
+const { JWT_SECRET } = process.env;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 router.get("/health", async (req, res, next) => {
-    // code here
+    res.send(`<h1>All is well, server is up!</h1>`)
 });
 
 router.use("/users", usersRouter);
 router.use("/activities", activitiesRouter);
 router.use("/routines", routinesRouter);
 router.use("/routineActivities", routineActivitiesRouter);
+
+router.use((error, req, res, next) => {
+    res.send({
+        name: error.name,
+        message: error.message
+    });
+});
 
 module.exports = router;
